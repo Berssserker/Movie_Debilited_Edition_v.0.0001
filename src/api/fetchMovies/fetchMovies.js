5ebf5539e56b951d947f0874c246d46e97e9604d
Semion <razdumkov1804@gmail.com>
2024-12-23 20:50:43 +0300

    asdasd

--- src/api/fetchMovies/fetchMovies.js
+++ src/api/fetchMovies/fetchMovies.js
@@ -1,10 +1,7 @@
-import { delay } from '../../utils/delay'
-
 import { getMovie } from './getMovie'
 
 export const fetchMovies = async (setError, setMoviesData, setLoading, text, page) => {
   setError(false)
-  await delay(3000)
   const body = await getMovie(text, page)
   setMoviesData(body.results || [])
   setLoading(false)
